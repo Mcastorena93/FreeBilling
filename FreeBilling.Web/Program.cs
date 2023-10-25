ac184e2e457a80b606f9cc251f0cd673f4490e41
Mcastorena93 <mauriciocastorena29@gmail.com>
2023-10-25 15:49:07 -0400

    Razor pages set up

--- FreeBilling.Web/Program.cs
+++ FreeBilling.Web/Program.cs
@@ -1,8 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Razor pages dependency injections 
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 
+if (builder.Environment.IsDevelopment())
+{
+    // Shows developer page if exception happens
+    app.UseDeveloperExceptionPage();
+}
+//Allows us to serve index.html as the default webpage
+app.UseDefaultFiles();
+
+
+//Allows us to serve files from wwwroot.
 app.UseStaticFiles();
+
+
+app.MapRazorPages();
 
 //app.run(async ctx =>
 //{
