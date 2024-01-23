ac4ed24850694abba81b5f69477e70f743b854de
Mcastorena93 <mauriciocastorena29@gmail.com>
2024-01-23 18:32:37 -0500

    computed values and formatting in vue

--- FreeBilling.App/src/App.vue
+++ FreeBilling.App/src/App.vue
@@ -1,4 +1,60 @@
 <script setup>
+    import { ref, reactive, computed } from "vue";
+    import { formatMoney } from "./formatters";
+
+    const name = ref("Shawn");
+
+    const nancy = ref("Nancy Smith");
+
+    const bills = reactive([
+        {
+            "hoursWorked": 15.5,
+            "rate": 325.00,
+            "date": "2023-05-05",
+            "work": "I did a thing...",
+            "customerId": 1,
+            "employeeId": 1
+        },
+        {
+            "hoursWorked": 20,
+            "rate": 105.00,
+            "date": "2023-05-06",
+            "work": "I did another thing thing...",
+            "customerId": 1,
+            "employeeId": 1
+        },
+        {
+            "hoursWorked": 15.5,
+            "rate": 140.00,
+            "date": "2023-05-07",
+            "work": "I finish a thing...",
+            "customerId": 1,
+            "employeeId": 1
+        }
+    ]);
+
+    const total = computed(() => {
+        return bills.map(b => b.rate * b.hoursWorked)
+            .reduce((b, t) => t + b, 0);
+    });
+
+    function changeMe() {
+        name.value += "+";
+        //alert(name);
+    }
+
+    function newItem() {
+        bills.push({
+           customerId: 1,
+           employeeId: 1,
+           hoursWorked: 5.0,
+           rate: 114,
+           work: "More work",
+           billDate: "2023-05-08"
+        });
+
+        console.log(bills.length);
+    }
 
 </script>
 
@@ -8,7 +64,28 @@
   </header>
 
   <main>
-    <h1>Hello from vue</h1>
+    <h1>Hello from Vue</h1>
+      <div>{{ name }}</div>
+      <button class="btn" @click="changeMe">Change Me</button>
+      <img src="" :alt="nancy" :title="nancy" />
+      <button class="btn" @click="newItem">New Item</button>
+      <table>
+          <thead>
+              <tr>
+                  <td>Hours</td>
+                  <td>Date</td>
+                  <td>Description</td>
+              </tr>
+          </thead>
+          <tbody>
+              <tr v-for="b in bills">
+                  <td>{{ b.hoursWorked}}</td>
+                  <td>{{ b.date}} </td>
+                  <td>{{ b.work}} </td>
+              </tr>
+          </tbody>
+      </table>
+      <div> Total: {{formatMoney(total)}} </div>
   </main>
 </template>
 
